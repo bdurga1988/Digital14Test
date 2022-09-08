5f9f27de741a7ed19b192b8b595882e33629d28d
bdurga1988 <113099110+bdurga1988@users.noreply.github.com>
2022-09-08 11:11:22 +0100

    Update WriterTest.java
    Updated file for both writers test

--- Digital14Test/src/main/java/org/example/WriterTest.java
+++ Digital14Test/src/main/java/org/example/WriterTest.java
@@ -6,15 +6,22 @@ public class WriterTest {
 
     public void writeToWriter(Writer writer) {
         writer.write("new new stupid content !!!");
-        writer.write(" good job job durga");
+        writer.write(" task for digital14");
         writer.close();
-        writer.write(" maha maha");
+        writer.write(" digital14");
     }
 
     public static void main(String[] args) throws IOException {
 
         WriterTest test = new WriterTest();
+        //Filewriter
         Writer writer = new FileWriter();
+        test.writeToWriter(writer);
+        writer.stupidRemover().uppercase().duplicateRemover();
+        System.out.println(writer.get());
+
+        //StringWriter
+        writer = new StringWriter();
         test.writeToWriter(writer);
         writer.stupidRemover().uppercase().duplicateRemover();
         System.out.println(writer.get());
